33aca12cd282b19317a9f7517f36fe21efa26f24
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-06-02 23:19:52 -0400

    Update registerCommands.js

--- registerCommands.js
+++ registerCommands.js
@@ -1,11 +1,6 @@
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 require('dotenv').config();
 
-if (!process.env.DISCORD_TOKEN || !process.env.CLIENT_ID) {
-    console.error("Missing DISCORD_TOKEN or CLIENT_ID in environment variables.");
-    process.exit(1);
-}
-
 const commands = [
     new SlashCommandBuilder()
         .setName('setchannel')
@@ -15,8 +10,11 @@ const commands = [
                 .setDescription('The channel to post new content in')
                 .setRequired(true)),
     new SlashCommandBuilder()
-        .setName('posts')
-        .setDescription('Fetch and display the latest posts'),
+        .setName('latest')
+        .setDescription('Fetch and display the latest post'),
+    new SlashCommandBuilder()
+        .setName('alldrops')
+        .setDescription('Fetch and display all posts'),
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
@@ -32,6 +30,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
-        console.error('Error reloading application (/) commands:', error);
+        console.error(error);
     }
 })();
